d282fbade652e24962e2e0d3f681101e474addaa
amota <amota@ROGSYS>
2022-07-07 20:02:56 -0400

    route -> controller -> response -> profit

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,10 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
 class VinylController
 {
-    public function homepage()
+    #[Route('/')]
+    public function homepage(): Response
     {
-        die('Vinyl: Definitely NOT a fancy-looking frisbee!');
+        return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
     }
 }
